6404fc2a85650ad74addf42443a2830d1373687b
Ion Mocan <ionatanmocan@gmail.com>
2021-08-22 16:22:25 +0300

    fix typos

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import OtherPage from './OtherPage';
@@ -8,11 +9,23 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <Link to="/">Home</Link>
-        <br></br>
-        <Link to="/otherpage">Other page</Link>
-        <Route exact path="/" component={Fib} />
-        <Route path="/otherpage" component={OtherPage} />
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <Link to="/">Home</Link>
+          <Link to="/otherpage">Other Page</Link>
+        </header>
+        <div>
+          <Route exact path="/" component={Fib} />
+          <Route path="/otherpage" component={OtherPage} />
+        </div>
       </div>
     </Router>
   );
